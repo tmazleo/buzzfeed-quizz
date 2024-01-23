a997674c4b6cd5a4b82d4be86761e3c622e5171b
tmazleo <leotomazb@icloud.com>
2024-01-23 12:28:45 -0300

    proijeto com logica

---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quizz',
+  templateUrl: './quizz.component.html',
+  styleUrls: ['./quizz.component.css']
+})
+export class QuizzComponent implements OnInit{
+  title:string = ""
+  questions:any
+  questionSelected:any
+
+  answers:string[] = []
+  answerSelected:string = ""
+
+  questionIndex:number = 0
+  questionMaxIndex:number = 0
+
+  finished:boolean = false
+  ngOnInit(): void {
+
+
+  }
+}
